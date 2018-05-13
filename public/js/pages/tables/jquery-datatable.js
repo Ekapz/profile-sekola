4c1f00ecc1d1812cc89f23eb679621f01f8f05a9
Abdullah Hafizh <abdllhhafizh@gmail.com>
2018-05-13 23:55:30 +0700

    final table

--- public/js/pages/tables/jquery-datatable.js
+++ public/js/pages/tables/jquery-datatable.js
@@ -1,14 +1,68 @@
-$(function () {
-    $('.js-basic-example').DataTable({
-        responsive: true
-    });
-
-    //Exportable table
-    $('.js-exportable').DataTable({
+$(document).ready(function() {    
+    var table = $('table').DataTable( {
+        "language": {
+            "decimal":        "",
+            "emptyTable":     "Tidak ada data yang tersedia pada tabel ini",
+            "info":           "Menampilkan _START_ sampai _END_ dari _TOTAL_ entri",
+            "infoEmpty":      "Menampilkan 0 sampai 0 dari 0 entri",
+            "infoFiltered":   "(disaring dari _MAX_ entri keseluruhan)",
+            "infoPostFix":    "",
+            "thousands":      ",",
+            "lengthMenu":     "Tampilkan _MENU_ entri",
+            "loadingRecords": "Loading...",
+            "processing":     "Sedang memproses...",
+            "search":         "Cari:",
+            "zeroRecords":    "Tidak ditemukan data yang sesuai",
+            "paginate": {
+                "first":      "Pertama",
+                "last":       "Terakhir",
+                "next":       "Selanjutnya",
+                "previous":   "Sebelumnya"
+            },
+            "aria": {
+                "sortAscending":  ": aktifkan untuk mengurutkan kolom ascending",
+                "sortDescending": ": aktifkan untuk mengurutkan kolom descending"
+            }
+        },     
+        lengthChange: true,
         dom: 'Bfrtip',
         responsive: true,
         buttons: [
-            'copy', 'csv', 'excel', 'pdf', 'print'
+            {
+                extend: 'copy',
+                text: 'Salin',
+                exportOptions: {
+                    columns: 'th:not(:last-child)'
+                }
+            },
+            {
+                extend: 'csv',
+                text: 'Export CSV',
+                exportOptions: {
+                    columns: 'th:not(:last-child)'
+                }
+            },
+            {
+                extend: 'excel',
+                text: 'Export Excel',
+                exportOptions: {
+                    columns: 'th:not(:last-child)'
+                }
+            },
+            {
+                extend: 'pdf',
+                text: 'Export PDF',
+                exportOptions: {
+                    columns: 'th:not(:last-child)'
+                }
+            },
+            {
+                extend: 'print',
+                text: 'Print',
+                exportOptions: {
+                    columns: 'th:not(:last-child)'
+                }
+            },            
         ]
-    });
-});
+    } );    
+} );
--- resources/views/admin/admin.blade.php
+++ resources/views/admin/admin.blade.php
@@ -38,17 +38,7 @@
                 <th>Start date</th>
                 <th>Salary</th>
               </tr>
-            </thead>
-            <tfoot>
-              <tr>
-                <th>Name</th>
-                <th>Position</th>
-                <th>Office</th>
-                <th>Age</th>
-                <th>Start date</th>
-                <th>Salary</th>
-              </tr>
-            </tfoot>
+            </thead>            
             <tbody>
               <tr>
                 <td>Tiger Nixon</td>
