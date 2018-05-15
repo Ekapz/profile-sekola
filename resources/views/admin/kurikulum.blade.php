7eeaf22b0414ca88aa9637b38a16e52af96d73f1
Ekapz <rusun315@gmail.com>
2018-05-15 22:33:55 +0700

    Terevisi-DataTable

--- resources/views/admin/kurikulum.blade.php
+++ resources/views/admin/kurikulum.blade.php
@@ -2,10 +2,56 @@
 
 @section('content')
 <div class="block-header">
-<h2>Kurikulum</h2>
+  <h2>Kabupaten</h2>
 </div>
-<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal" data-whatever="@getbootstrap">Open modal for @getbootstrap</button>
-
+<button type="button" class="btn btn-primary pull-right" data-toggle="modal" data-target="#exampleModal" data-whatever="@getbootstrap">Tambah Data</button>
+<hr>
+<hr>
+<!-- Exportable Table -->
+<div class="row clearfix">
+  <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+    <div class="card">
+      <div class="header">
+        <h2>
+          EXPORTABLE TABLE
+        </h2>
+        <ul class="header-dropdown m-r--5">
+          <li class="dropdown">
+            <a href="javascript:void(0);" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+              <i class="material-icons">more_vert</i>
+            </a>
+            <ul class="dropdown-menu pull-right">
+              <li><a href="javascript:void(0);">Action</a></li>
+              <li><a href="javascript:void(0);">Another action</a></li>
+              <li><a href="javascript:void(0);">Something else here</a></li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+      <div class="body">
+        <div class="table-responsive">          
+          <table class="table table-bordered table-striped table-hover">
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>Kurikulum</th>
+                <th>Keterangan Kurikulum</th>
+              </tr>
+            </thead>            
+            <tbody>
+              <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- #END# Exportable Table -->
 <div class="modal fade" id="exampleModal" role="dialog" aria-labelledby="exampleModalLabel">
   <div class="modal-dialog" role="document">
     <div class="modal-content">
