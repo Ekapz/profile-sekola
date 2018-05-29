17705271107b3f2ad1ef7e69115c8f925d357b54
Abdullah Hafizh <abdllhhafizh@gmail.com>
2018-05-30 03:17:48 +0700

    sekilas sekolah

--- resources/views/cari.blade.php
+++ resources/views/cari.blade.php
@@ -10,12 +10,26 @@
                     @foreach($sekolahan as $sekolah)
                     <div class="col-md-4">
                         <div class="panel panel-default">
-                            <div class="panel-heading">{{ $sekolah->nama }}</div>
-                            <div class="panel-body">                                
-                                {{ $sekolah->desa->desa }}<br>
-                                {{ $sekolah->desa->kecamatan->kecamatan }}<br>
-                                {{ $sekolah->desa->kecamatan->kabupaten->kabupaten }}<br>
-                                {{ $sekolah->desa->kecamatan->kabupaten->provinsi->provinsi }}<br>
+                            <div class="panel-heading text-center"><b>{{ $sekolah->nama }}</b></div>
+                            <img src="{{ asset('uploads/'.$sekolah->image) }}" class="img-responsive">
+                            <div class="panel-body">
+                                <b>
+                                    <legend>Alamat</legend>
+                                    <p>
+                                        {{ $sekolah->alamat }}, Rt {{ $sekolah->rt }}/{{ $sekolah->rt }}, {{ $sekolah->desa->desa }}, {{ $sekolah->desa->kecamatan->kecamatan }}<br>
+                                        {{ $sekolah->desa->kecamatan->kabupaten->kabupaten }} - {{ $sekolah->desa->kecamatan->kabupaten->provinsi->provinsi }}
+                                    </p>
+                                    <legend>Kontak</legend>
+                                    <label>No. Telp : {{ $sekolah->no_telp }}</label><br>
+                                    <label>No. Fax : {{ $sekolah->no_fax }}</label><br>
+                                    <label>Website : <a href="{{ $sekolah->website }}" target="_blank">{{ $sekolah->website }}</a></label>
+                                    <legend>Guru</legend>
+                                    <label>Kepala Sekolah : {{ $sekolah->kepala_sekolah }}</label>
+                                    <?php
+                                    $jumlahguru = App\Guru::where('sekolah_id', '=', $sekolah->id)->count();
+                                    ?>
+                                    <label>Jumlah Guru : {{ $jumlahguru }}</label>
+                                </b>
                             </div>
                             <div class="panel-footer">
                                 <a href="#" class="btn btn-primary btn-block">Lihat</a>
